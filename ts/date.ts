dd359e0c7798f30580253cb90bb0b487bc21e390
Wilson Lin <code@wilsonl.in>
2024-04-12 12:44:41 +1000

    VUnixSecTimestamp, VUnixMsTimestamp

--- ts/date.ts
+++ ts/date.ts
@@ -20,3 +20,37 @@ export class VDate extends Validator<Date> {
     return parsed;
   }
 }
+
+export class VUnixSecTimestamp extends Validator<Date> {
+  public constructor(helper?: string) {
+    super(new Date(Date.UTC(2010, 4, 19, 16, 30, 45, 123)), helper);
+  }
+
+  parse(theValue: ValuePath, raw: unknown): Date {
+    if (typeof raw != "number") {
+      throw theValue.isBadAsIt("is not a UNIX timestamp in seconds");
+    }
+    const parsed = new Date(raw * 1000);
+    if (isNaN(parsed.getTime())) {
+      throw theValue.isBadAsIt(`is not a valid timestamp`);
+    }
+    return parsed;
+  }
+}
+
+export class VUnixMsTimestamp extends Validator<Date> {
+  public constructor(helper?: string) {
+    super(new Date(Date.UTC(2010, 4, 19, 16, 30, 45, 123)), helper);
+  }
+
+  parse(theValue: ValuePath, raw: unknown): Date {
+    if (typeof raw != "number") {
+      throw theValue.isBadAsIt("is not a UNIX timestamp in milliseconds");
+    }
+    const parsed = new Date(raw);
+    if (isNaN(parsed.getTime())) {
+      throw theValue.isBadAsIt(`is not a valid timestamp`);
+    }
+    return parsed;
+  }
+}
